691e4435f87867463b3f7b14047c0914266be873
bryceharmsen <31521972+bryceharmsen@users.noreply.github.com>
2019-11-01 09:19:34 -0700

    Possibly finished. Need to test more.

--- nodeCalculator/rendering/calculator.js
+++ nodeCalculator/rendering/calculator.js
@@ -3,17 +3,109 @@ class Calculator {
         try {
             let expression = req.query.expression;
             let calc = new Calculator();
-            res.locals.result = await calc.evaluate(expression);
+            res.locals.result = await calc.calculate(expression);
             next();
         } catch (error) {
             console.error(error);
         }
     }
 
-    async evaluate(expression) {
+    async calculate(expression) {
         //split values/operators into array
         //run infix calculator
-        return expression;
+        let terms = expression.split(' ');
+        let result = await this.calculateInFix(terms);
+        return result;
+    }
+
+    async calculateInFix(terms) {
+        let numbers = [];
+        let operators = [];
+        let orderOfPrecedence = await this.getOrderOfPrecedence();
+        for (let i = 0; i < terms.length; i++) {
+            let term = terms[i];
+            await this.processNext(term, numbers, operators, orderOfPrecedence);
+        }
+
+        if (numbers.length > 1) {
+            while(operators.length > 0) {
+                let operator = operators.pop();
+                if (operator.includes('('))
+                    return false;
+                let result = await this.evaluate(numbers.pop(), numbers.pop(), operator);
+                numbers.push(result);
+            }
+        }
+
+        return numbers.pop();
+    }
+
+    async processNext(term, numbers, operators, orderOfPrecedence) {
+        let num = parseFloat(term);
+        if (''.includes(term))
+            return false;
+        if (!isNaN(num)) {
+            numbers.push(num);
+        } else {
+            await this.processOperator(term, numbers, operators, orderOfPrecedence);
+        }
+
+        console.log('next term: \n', numbers, '\n', operators);
+    }
+
+    async processOperator(term, numbers, operators, orderOfPrecedence) {
+        let prevOperator = operators[operators.length - 1];
+        while(operators.length > 0 &&
+                orderOfPrecedence.get(term) <=
+                orderOfPrecedence.get(prevOperator = operators[operators.length - 1])
+                    && !prevOperator.includes('(')) {
+            console.log('popping: \n', numbers, '\n', operators);
+            let result = await this.evaluate(numbers.pop(), numbers.pop(), operators.pop());
+            numbers.push(result);
+        }
+        if (prevOperator && prevOperator.includes('(') && term.includes(')')) {
+            operators.pop();
+        }
+        if (!term.includes(')')) {
+            operators.push(term);
+        }
+    }
+
+    async evaluate(bOperand, aOperand, operator) {
+        let result = 0;
+        switch(operator) {
+            case '^':
+                result = Math.pow(aOperand, bOperand);
+                break;
+            case '*':
+                result = aOperand * bOperand;
+                break;
+            case '/':
+                result = aOperand / bOperand;
+                break;
+            case '+':
+                result = aOperand + bOperand;
+                break;
+            case '-':
+                result = aOperand - bOperand;
+                break;
+            default:
+                break;
+        }
+
+        return result;
+    }
+
+    async getOrderOfPrecedence() {
+        let precedences = new Map();
+        precedences.set(')', 0);
+        precedences.set('+', 2);
+        precedences.set('-', 2);
+        precedences.set('/', 3);
+        precedences.set('*', 3);
+        precedences.set('^', 5);
+        precedences.set('(', 6);
+        return precedences;
     }
 }
 
